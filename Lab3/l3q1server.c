21db80e9e096df196999b5066135f4b587fa36cc
Satwik <113183355+satwikambashta@users.noreply.github.com>
2023-08-24 18:51:56 +0530

    Update l3q1server.c

--- Lab3/l3q1server.c
+++ Lab3/l3q1server.c
@@ -1,34 +1,48 @@
 /*Write a UDP client-server program where t he client sends rows of a matrix, and the
 servercombines them together as a matrix*/
-
-#include <stdio.h>
-#include <fcntl.h>
-#include <string.h>
-#include <stdlib.h>
-#include <sys/socket.h>
 #include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
-int main()
-{
-    int sd;
-    char buf[25];
-    struct sockaddr_in sadd, cadd;
-    sd = socket(AF_INET, SOCK_DGRAM, 0);
-    sadd.sin_family = AF_INET;
-    sadd.sin_addr.s_addr = inet_addr("172.16.59.75"); 
-    sadd.sin_port = htons(9704);
-    int len = sizeof(cadd);
-    int result = bind(sd, (struct sockaddr *)&sadd, sizeof(sadd));
-    int m;
+#include <fcntl.h>
 
-    recvfrom(sd, buf, sizeof(buf), 0, (struct sockaddr *)&cadd, &len);
-    printf("%d",m);
+#define PORTNO 10200
 
-    // int m = recvfrom(sd, buf, sizeof(buf), 0, (struct sockaddr *)&cadd, &len);
-    // printf("the server send is\n");
-    // puts(buf);
-    // int n = sendto(sd, buf, sizeof(buf), 0, (struct sockaddr *)&cadd, len);
-    return 0;
+void main() {
+    int status, rows, cols, clientAddrLen;
+    struct sockaddr_in serverAddr, clientAddr;
+    int sockID = socket(AF_INET, SOCK_DGRAM, 0);
+
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = inet_addr("172.16.59.33"); 
+    serverAddr.sin_port = htons(PORTNO);
+    bind(sockID, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
+
+    int arr[100];
+
+    recvfrom(sockID, arr, sizeof(arr), 0, (struct sockaddr *)&clientAddr, &clientAddrLen);
+
+    rows = arr[0];
+    cols = arr[1];
+
+    int mat[rows][cols]; // Using a static 2D array
+
+    for (int i = 0; i < rows; i++) {
+        recvfrom(sockID, arr, sizeof(arr), 0, (struct sockaddr *)&clientAddr, &clientAddrLen);
+        for (int j = 0; j < cols; j++)
+            mat[i][j] = arr[j];
+    }
+
+    printf("The matrix formed is: \n");
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++)
+            printf("%d ", mat[i][j]);
+        printf("\n");
+    }
+
 }
